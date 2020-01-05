cbc01e95137730b35477dc38cca8a1fa1a673ad3
jayShepard <jayden.shepard@alayacare.com>
2020-01-05 15:30:29 -0500

    Bug fixes

--- Sudoku.py
+++ Sudoku.py
@@ -1,19 +1,19 @@
 import random
 
-from cell import Cell
+from cell import Cell, Membership
 from enum import Enum
 from typing import List
+
+class Difficulty:
+    EASY = 'easy'
+    MEDIUM = 'medium'
+    HARD = 'hard'
 
 class Sudoku:
     """
     The Sudoku game. Initializes with no cells. Call method initialize_game to populate
     the instance with a valid, populated Sudoku grid.
     """
-
-    class Difficulty(Enum):
-        EASY = 'easy'
-        MEDIUM = 'medium'
-        HARD = 'hard'
 
     def __init__(self):
         self.cells: List[Cell] = list() 
@@ -33,14 +33,14 @@ class Sudoku:
             for current_node in node_buff:
                 for other_node in node_buff:
                     if current_node != other_node:
-                        current_node.add_memember_to(Cell.Membership.ROW, other_node)
+                        current_node.add_memember_to(Membership.ROW, other_node)
 
             # set columns
             node_buff = cells[0+ i: 73 + i: 9]
             for current_node in node_buff:
                 for other_node in node_buff:
                     if current_node != other_node:
-                        current_node.add_memember_to(Cell.Membership.COLUMN, other_node)
+                        current_node.add_memember_to(Membership.COLUMN, other_node)
 
             # set groups
             if i % 3 == 0 and i > 0:
@@ -53,7 +53,7 @@ class Sudoku:
             for current_node in node_buff:
                 for other_node in node_buff:
                     if current_node != other_node:
-                        current_node.add_memember_to(Cell.Membership.GROUP, other_node)
+                        current_node.add_memember_to(Membership.GROUP, other_node)
     
         return cells
 
@@ -87,27 +87,27 @@ class Sudoku:
         self.set_values_for_cells(cells)
         self.cells = cells
 
-    def get_grid(self, difficulty: Difficulty) -> List[int]:
+    def get_grid(self, difficulty: str) -> List[int]:
         """
         Returns a flat list of integers representing the game. 0 is an empty cell
         """
-        if difficulty == self.Difficulty.EASY:
+        if difficulty == Difficulty.EASY:
             number_of_cells = 10
-        elif difficulty == self.Difficulty.MEDIUM:
+        elif difficulty == Difficulty.MEDIUM:
             number_of_cells = 7
-        elif difficulty == self.Difficulty.HARD:
+        elif difficulty == Difficulty.HARD:
             number_of_cells = 5
         
         given_cells = random.choices(range(0, 81), k=number_of_cells)
         
-        blah = []
+        game_grid = []
         for i, cell in enumerate(self.cells):
             if i in given_cells:
-                blah.append(cell.value or 0)
+                game_grid.append(cell.value or 0)
             else:
-                blah.append(0)
+                game_grid.append(0)
         
-        return blah
+        return game_grid
 
     def validate_solution(self, player_solution: List[int]) -> bool:
         """
@@ -127,3 +127,12 @@ class Sudoku:
             print(f'| {x.value} ', end = '')
         print("|")
         print('-------------------------------------')
+
+    def get_solved(self) -> List[int]:
+        return [cell.value or 0 for cell in self.cells]
+
+
+sudoku = Sudoku()
+sudoku.initialize_game()
+grid = sudoku.get_grid(Difficulty.EASY)
+print(grid)
--- cell.py
+++ cell.py
@@ -1,18 +1,17 @@
 import random
 
-from enum import Enum
 from typing import List, Optional, Union    
+
+class Membership:
+    ROW = "row"
+    COLUMN = "column"
+    GROUP = "group"
 
 class Cell:
     """
     A doubly linked cell for a sudoku board. Each cell contains references to every other
     Cell in its member row, column, and group.
     """
-
-    class Membership(Enum):
-        ROW = "row"
-        COLUMN = "column"
-        GROUP = "group"
 
     def __init__(self, value: Optional[int] = None):
         self.value: Union[int, None] = value
@@ -23,15 +22,15 @@ class Cell:
     def __repr__(self):
         return f"Node(value: {self.value})"
 
-    def add_memember_to(self, membership: Membership, member: Cell):
+    def add_memember_to(self, membership: str, member: "Cell"):
         """
         Mutually adds a member to a Cell's column, row, or group, and adds the current
         Cell to the member
         """
-        if member not in getattr(self, str(membership)):
-            getattr(self, str(membership)).append(member)
-        if self not in getattr(member, str(membership)):
-            getattr(member, str(membership)).append(self)
+        if member not in getattr(self, membership):
+            getattr(self, membership).append(member)
+        if self not in getattr(member, membership):
+            getattr(member, membership).append(self)
 
     def clear_value(self):
         self.value = None
@@ -43,9 +42,9 @@ class Cell:
         """
         starting_set = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-        for node in set(self.column + self.row + self.group):
-            if node.value in starting_set:
-                starting_set.remove(node.value)
+        for cell in set(self.column + self.row + self.group):
+            if cell.value in starting_set:
+                starting_set.remove(cell.value)
         
         if len(starting_set) > 1:
             self.value = starting_set[random.randint(0, len(starting_set) - 1)]
